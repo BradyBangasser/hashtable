cac518b4823d3dc4c9ee041320bca4838b77dee1
BradyBangasser <bradybbangasser@gmail.com>
2025-02-25 22:45:35 -0600

    continued work

--- hashtable.c
+++ hashtable.c
@@ -1,23 +1,56 @@
 #define HT_TEST
 
 #include <memory.h>
+#include <stdlib.h>
 #ifdef HT_TEST
 #include <stdio.h>
 #endif
 
 #include "hashtable.h"
 
+#define HT_INSERT_ELEMENT(heap, hv, v) ({ \
+        assert(sizeof(*heap) == sizeof(struct ht)); \
+        uint64_t i; \
+        for (i = hv; heap->buckets[i] && !heap->buckets[i].grave; i = (i + 1) % heap->size); \
+        heap->buckets[i].payload = v; \
+        }) \
 
 typedef struct ht_bucket {
     void *payload;
+    uint8_t grave;
 } ht_bucket;
 
 uint8_t ht_init(ht *ht, ht_allocation_type alloc_type) {
     memset(ht, 0, sizeof(*ht));
 
     if (alloc_type == ALLOC_EAGER) {
+        ht->buckets = calloc(HT_INITIAL_BUCKETS, sizeof(ht_bucket));
+        if (ht->buckets == NULL) {
+            return 1;
+        }
+    }
 
     return 0;
+}
+
+static inline uint8_t _ht_resize_table(ht *ht, uint64_t s) {
+    uint64_t i;
+    ht_bucket *b = ht->buckets;
+    ht_bucket *curs;
+
+    ht->buckets = calloc(sizeof(ht_bucket), s);
+    if (ht->buckets == NULL) return 0;
+
+    if (ht->n) {
+        curs = ht->buckets + ht->size;
+        do {
+            curs = curs - 1;
+            if (curs) {
+
+uint8_t ht_insert(ht *ht, void *p) {
+    if (ht->alloc_type == ALLOC_LAZY && (ht->size == 0 || ((ht->n * 10) / (ht->size)) > HT_MAX_LOAD_FACTOR)) {
+
+    }
 }
 
 #ifdef HT_TEST
--- hashtable.h
+++ hashtable.h
@@ -3,20 +3,25 @@
 
 #include <inttypes.h>
 
+#define HT_INITIAL_BUCKETS 10
+#define HT_MAX_LOAD_FACTOR 7
+#define HT_MIN_LOAD_FACTOR 3
+
 typedef struct ht_bucket ht_bucket;
 typedef uint64_t (*ht_hfn)(void *);
+
+typedef enum {
+    ALLOC_EAGER,
+    ALLOC_LAZY,
+} ht_allocation_type;
 
 typedef struct ht {
     const ht_hfn hfn;
     ht_bucket *buckets;
+    ht_allocation_type alloc_type;
 
     uint64_t size;
     uint64_t n;
 } ht;
-
-typedef enum __attribute__((packed)) {
-    ALLOC_EAGER,
-    ALLOC_LAZY,
-} ht_allocation_type;
 
 #endif
